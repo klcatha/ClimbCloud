883c2cac83a1840315fe62a6b982e1ced75413d0
klcatha <klcatha0518@gmail.com>
2018-10-24 07:49:49 +0900

    camera move

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+	GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		
+		this.player = GameObject.Find("cat");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 playerPos = this.player.transform.position;
+		transform.position = new Vector3(
+			transform.position.x, transform.position.y, transform.position.z);
 	}
 }
