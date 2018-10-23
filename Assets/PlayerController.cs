ed9d90e45199b8fd96a36228eaa22da8a2ff8c0d
klcatha <klcatha0518@gmail.com>
2018-10-23 10:07:43 +0900

    add force jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	Rigidbody2D rigid2D;
+	float jumpForce = 780.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+		this.rigid2D = GetComponent<Rigidbody2D>();		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// ジャンプする
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			this.rigid2D.AddForce(transform.up * this.jumpForce);
+		}
 	}
 }
