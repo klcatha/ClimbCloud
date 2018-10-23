949abdcbc86fd02934308a205c2764ca18cce301
klcatha <klcatha0518@gmail.com>
2018-10-23 10:35:58 +0900

    animator speed control

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	Rigidbody2D rigid2D;
+	Animator animator;
 	float jumpForce = 780.0f;
 	float workForce = 30.0f;
 	float maxWalkSpeed = 2.0f;
 
 	// Use this for initialization
 	void Start () {
-		this.rigid2D = GetComponent<Rigidbody2D>();		
+		this.rigid2D = GetComponent<Rigidbody2D>();
+		this.animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -41,5 +43,8 @@ public class PlayerController : MonoBehaviour {
 		{
 			transform.localScale = new Vector3(key, 1, 1);
 		}
+
+		// プレイヤの速度に応じてアニメーション速度を変える
+		this.animator.speed = speedx / 2.0f;
 	}
 }
