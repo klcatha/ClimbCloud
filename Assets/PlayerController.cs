c703463b8ebb4dd62419b7ea7c3a43d0484bb187
klcatha <klcatha0518@gmail.com>
2018-10-24 08:33:57 +0900

    transition game clear scene

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -61,5 +62,6 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		Debug.Log("ゴール");
+		SceneManager.LoadScene("ClearScene");
 	}
 }
